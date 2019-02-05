609f4705b70206e0d724c52ac7069f79812fb6e4
DomPou <45173284+DomPou@users.noreply.github.com>
2019-02-05 14:11:51 -0500

    Created Moving_features_from_proposed_trails_to_registered_trails_v2 and functions to get information directly from Salesforce

---
+++ Salesforce/Salesforce_connection_variables.py
@@ -0,0 +1,5 @@
+# Salesforce connection properties
+userSF = 'dpoulin@tctrail.ca'
+passwordSF = 'A2XlW42tw9SL'
+securityToken = '6KxCh6z7CegggqBx5HMeG67Fs'
+passwordTOKEN = passwordSF + securityToken
